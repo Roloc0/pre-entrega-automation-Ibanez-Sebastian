223f6906e168799a71570e15678ecac754c99a7d
Roloc0 <sebastianibanez95@gmail.com>
2025-10-20 14:00:42 -0300

    subida de los primeros archivos de la pre-entrega

---
+++ test/prueba.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+@pytest.fixture
+
+
+# abrir el navegador
+# dirigirse a la pagina "saucedemo.com"
+# ingresar usuario y contraseña
+# validar login exitoso
+def test_login ():
+
+
+# hacer login en la pagina
+# verificar el titulo de la pagina
+# comprobar si hay productos visibles
+def test_catalogo():
+
+
+# hacer login en la pagina
+# añadir un producto al carrito
+# ingresar al carrito
+# comprobar que el producto este en el carrito
+def test_carrito ():
